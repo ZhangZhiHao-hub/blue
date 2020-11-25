9aa067b6b537f1e93be872cef92e94b1dd9d1de6
ZhangZhiHao-hub <584435748@qq.com>
2020-11-25 18:27:54 +0800

    考试1

---
+++ 考试/1/考试所需框架和图片/AI/src/AI.java
@@ -0,0 +1,38 @@
+import javax.microedition.lcdui.*;
+import javax.microedition.midlet.*;
+import java.io.*;
+
+public class AI extends MIDlet
+{
+	Display display;
+	MainCanvas mc;
+	public AI(){
+		display=Display.getDisplay(this);
+		mc=new MainCanvas();
+		display.setCurrent(mc);
+	}
+	public void startApp(){
+	}
+	public void destroyApp(boolean unc){
+	}
+	public void pauseApp(){
+	}
+}
+class MainCanvas extends Canvas
+{
+	public MainCanvas(){
+		try{
+			
+		}
+		catch(IOException e){
+			e.printStackTrace();
+		}
+	}
+	public void paint(Graphics g){
+		g.setColor(250,200,180);
+		g.fillRect(0,0,getWidth(),getHeight());
+	}
+	public void keyPressed(int keyCode){
+		int action=getGameAction(keyCode);
+	}
+}
